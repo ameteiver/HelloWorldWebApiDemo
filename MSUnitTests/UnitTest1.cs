e5b15cc9f50c7976b41d32aceca0cc754f2e707d
Ameteiver <ameteiver@gmail.com>
2018-07-13 17:59:34 -0400

    refined unit testing

--- MSUnitTests/UnitTest1.cs
+++ MSUnitTests/UnitTest1.cs
@@ -1,5 +1,8 @@
 using HelloWorldAPI.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http;
+using HelloWorldAPI.Model;
 
 namespace MSUnitTests
 {
@@ -7,11 +10,28 @@ namespace MSUnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetHelloWorldTest()
         {
             MessageController messageController = new MessageController();
             var message = messageController.GetHelloWorld();
-            Assert.Equals("Hello Wld", message);
+            Assert.Equals("Hello World", message);
+        }
+        [TestMethod]
+        public async System.Threading.Tasks.Task WebGetHelloWorldTestAsync()
+        {
+            HttpClient client = new HttpClient();
+            var uri = new Uri(string.Format("http://demoapihelloworld.azurewebsites.net/api/message", string.Empty));
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var message = new Message(content);
+                Assert.AreEqual("Hello World", message.ToString());
+            }
+            else
+            {
+                Assert.Fail("Something might be wrong with the web serivce!");
+            }
         }
     }
 }
